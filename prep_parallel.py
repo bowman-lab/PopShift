d8c0b80a8e543bd1cde305e41f27eee0d35df1c8
Borna Novak <bnovak@wustl.edu>
2022-03-27 12:42:04 -0500

    changed to argparse and added extra arguments

--- prep_parallel.py
+++ prep_parallel.py
@@ -9,12 +9,12 @@
 
 #TODO might be interesting to add the function of adding amber charges instead of only vina charges to the protein
 
-import numpy as np
 import os
 import subprocess as sp
 import sys
 import mdtraj as md
-from optparse import OptionParser
+#from optparse import OptionParser
+import argparse
 import glob
 import multiprocessing as mp
 
@@ -28,28 +28,36 @@ def prep_receptor(receptor, out, name):
     sp.run(['%s/prepare_receptor' % executables, '-r', receptor, '-o', '%s/%sqt' % (out, name)])
     return '%s/%sqt' % (out, name)
 
-def align(protein_file,reference_file,output):
+def align(protein_file,reference_file,output, atoms):
     pdb = md.load(protein_file)
-    atoms = pdb.topology.select('backbone') #TODO change this to be an input with backbone being default (mdtraj syntax to be input) - input could be different; such as aligning different pockets between runs
+    atoms = pdb.topology.select(atoms)
+    print('Aligning using %s' % atoms)
     aligned = pdb.superpose(reference_file, atom_indices=atoms)
     prot_name = protein_file.split('/')[-1].split('.')[0]
     aligned.save_pdb('%s/%s_aligned.pdb' % (output,prot_name))
     return '%s/%s_aligned.pdb' % (output, prot_name)
 
-#TODO change the parser to argparse
-parser = OptionParser()
-parser.add_option('-l','--ligand_dir', dest='ligand_dir', help='Path to ligand directory')
-parser.add_option('-p','--protein_dir', dest='protein_dir', help='Path to protein directory')
-parser.add_option('-a','--align', dest='aligning', help='Do you want to align?', default='no')
-parser.add_option('-r','--reference', dest='reference', help='Reference structure to align to', default='None')
-parser.add_option('-o','--out', dest='output', help='Path to the output')
+parser = argparse.ArgumentParser()
+parser.add_argument('protein_dir',
+                    help='Path to protein directory')
+parser.add_argument('output',
+                    help='Path to the output')
+parser.add_argument('-l','--ligand_dir',
+                    help='Path to ligand directory')
+parser.add_argument('-a','--align', default='no',
+                    choices=['yes','no'],
+                    help='Whether to align the structures. Default: no')
+parser.add_argument('-r','--reference', default='None',
+                    help='Path to reference structure to use for aligning')
+parser.add_argument('-x','--atoms', default='backbone',
+                    help='What atoms to use for aligning. Default: backbone')
 
-(options, args) = parser.parse_args()
-path_lig = options.ligand_dir
-path_prot = options.protein_dir
-path_output = options.output
-to_align = options.aligning
-reference = options.reference
+args = parser.parse_args()
+path_lig = args.ligand_dir
+path_prot = args.protein_dir
+path_output = args.output
+to_align = args.align
+reference = args.reference
 
 protein_name = path_output.split('/')[-1]
 n_procs = mp.cpu_count()
@@ -59,6 +67,11 @@ try:
     os.makedirs('%s' % path_output)
 except FileExistsError:
     pass
+
+if args.ligand_dir is not None:
+    print('ligand_dir is set') #! This is where setting up ligands should go
+else:
+    print('not set')
 
 #TODO prep_ligand and prep_protein should be combines; prep_ligand can only be done if a certain flag exists --ligand yes
 #for ligand in sorted(glob.glob('%s/*mol2' % path_lig)):  #Having pdb here should only be a temporary solution, make sure to change this and also take mol2
@@ -70,10 +83,10 @@ if to_align == 'yes':
     reference_protein = md.load(reference)
     frames = sorted(glob.glob('%s/*pdb' % path_prot))
     prot_name = [frame.split('/')[-1] for frame in frames]
-    output_list = [i for i in [path_output] for l in range(len(frames))]
-    print(output_list)
+    output_list = [path_output for l in range(len(frames))]
     reference_protein = [i for i in reference_protein for l in range(len(frames))]
-    arguments = zip(frames, reference_protein, output_list)
+    atoms = [args.atoms for l in range(len(frames))]
+    arguments = zip(frames, reference_protein, output_list,atoms)
     aligned = list(pool.starmap(align, arguments))
     arguments = zip(aligned, output_list, prot_name)
     list(pool.starmap(prep_receptor, arguments))
