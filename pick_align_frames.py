2a412d01db1d200d375f244546ae64319b32ffc0
louissmith <louis.smith88@gmail.com>
2022-04-07 16:45:08 -0500

    First complete draft of frame picker. Does not sort indices.

--- pick_align_frames.py
+++ pick_align_frames.py
@@ -1,28 +1,70 @@
 import argparse
 import numpy as np
 from enspara import ra
+import loos
+from loos import pyloos
 
 
 def get_assign_inds(assignments, nstates):
-    traj_ind_list = [[] for i in range(nstates)]
-    frame_ind_list = [[] for i in range(nstates)]
+    index_list_by_bin = [[] for i in range(nstates)]
     for i, t in enumerate(assignments):
         for j, f in enumerate(t):
             # iterating over RA assign contents produces singleton arrays of dtype=int32
             # that need to be unpacked for use
-            ind = f[0]
-            traj_ind_list[ind].append(i)
-            frame_ind_list[ind].append(j)
-    return [np.array([t_inds, f_inds], dtype=np.int32)
-            for t_inds, f_inds in zip(traj_ind_list, frame_ind_list)]
+            bin_index = f[0]
+            index_list_by_bin[bin_index].append((i, j))
+    return [np.array(bin_indices, dtype=[('traj', np.int32), ('frame', np.int32)]) for bin_indices in index_list_by_bin]
 
 
 def get_random_per_bin(assignments, n_states, number_desired, replace=False, gen=np.random.default_rng()):
     # assumes RA's traj axis is the outer one.S
     all_state_indices = get_assign_inds(assignments, n_states)
-    selected_inds = np.array([gen.choice(state_ixs, number_desired, axis=1, replace=replace)
-                              for state_ixs in all_state_indices], dtype=[('bin', np.int32), ('traj', np.int32), ('frame', np.int32)])
-    return selected_inds
+    chosen_inds = np.array([gen.choice(state_ixs, number_desired, axis=0, replace=replace)
+                              for state_ixs in all_state_indices])
+    return chosen_inds
+
+
+def rip_conformations(chosen_inds, model, subset_selection, align_selection, out_path, traj_paths):
+    trajectories = [pyloos.Trajectory(traj_path, model, subset=subset_selection) for traj_path in traj_paths[1:]]
+    full_inds = []
+    subset_list = []
+    align_list = []
+    bin_trj_file_names = [loos.DCDWriter(out_path+'/'+str(i)+'/samples.dcd') for i in range(chosen_inds.shape[0])]
+    # cflat = chosen_inds.flatten()
+    # chosen_sort_inds = np.argsort(cflat, order=['traj', 'frame'])
+    # inverse_sort_inds = np.zeros_like(chosen_sort_inds, dtype=np.int32)
+    # inverse_sort_inds[chosen_sort_inds] = np.arange(inverse_sort_inds.shape[0], dtype=np.int32)
+
+    # read selected frames into memory
+    for bin_ix, samples in enumerate(chosen_inds):
+        for trj_ix, fra_ix in samples:
+            frame = trajectories[trj_ix].readFrame(fra_ix)
+            align_list.append(loos.selectAtoms(frame, align_selection))
+            subset_list.append(frame)
+            full_inds.append((bin_ix, trj_ix, fra_ix))
+
+    xforms, rmsd, iters = pyloos.iterativeAlignment(align_list)
+    # apply transforms here
+    loos.applyTransforms(subset_list, xforms)
+    print('Iterative alignment final RMSD', rmsd, 'after', iters, 'iterations')
+
+    # now write everything out
+    for (bin_ix, trj_ix, fra_ix), frame in zip(full_inds, subset_list):
+        bin_trj_file_names[bin_ix].writeFrame(frame),
+        pdb = loos.PDB.fromAtomicGroup(frame)
+        pdbfname =out_path + '/{}/{}-{}.pdb'.format(bin_ix, trj_ix, fra_ix)
+        with open(pdbfname, "w") as f:
+            f.write(str(pdb))
+
+
+
+
+
+
+
+
+
+
 chosen = get_random_per_bin(d, 200, 3, gen=gen)
 
 def read_traj_paths(traj_list_filename):
@@ -34,6 +76,10 @@ frame_selectors = {
 }
 
 parser = argparse.ArgumentParser()
+parser.add_argument('receptor_name', type=str,
+                    help="Name to use to use as top-level directory for the docking run tree.")
+parser.add_argument('model', type=str,
+                    help='A loos-interpretable model file that will permit reading the trajectories in "traj_paths".')
 parser.add_argument('assignments', type=str,
                     help='h5 file with assignments associated to the MSM used.')
 parser.add_argument('eq_probs', type=str,
@@ -41,10 +87,10 @@ parser.add_argument('eq_probs', type=str,
 parser.add_argument('traj_paths', type=str,
                     help='A file containing a list of trajectories corresponding (in matching order) to the supplied '
                          'assignments file.')
-parser.add_argument('model', type=str,
-                    help='A loos-interpretable model file that will permit reading the trajectories in "traj_paths".')
-parser.add_argument('selection', type=str,
+parser.add_argument('align_selection', type=str,
                     help='A loos selection string to align the selected frames with.')
+parser.add_argument('--subset-selection', type=str, default='all',
+                    help='A loos selection string to subset all frames by (for example, if water is present it could be stripped here).')
 parser.add_argument('frame_selector', type=str,
                     choices=frame_selectors.keys())
 parser.add_argument('--frames-per-bin', type=int, default=10,
@@ -58,9 +104,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
     assignments = ra.load(args.assignments)
     eq_probs = np.load(args.eq_probs)
+    model = loos.createSystem(args.model)
     chosen_frames = frame_selectors[args.frame_selector](
         assignments,
         eq_probs.shape[0],
         args.frames_per_bin)
     traj_paths = read_traj_paths(args.traj_paths)
-    for
+    rip_conformations(chosen_frames, model, args.subset_selection, args.align_selection, args.receptor_name+'/receptor',
+                      traj_paths)
+
