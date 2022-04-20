45b86b79a7f2d0661c25d005bdb9ff4c1a54c619
louissmith <louis.smith88@gmail.com>
2022-04-20 18:15:10 -0500

    First full draft. Extracted paths from various methods.

--- docking_parallel.py
+++ docking_parallel.py
@@ -1,14 +1,10 @@
-from jug import TaskGenerator
+from jug import TaskGenerator, Task
 from vina import Vina
-from functools import partial
-import os
 import argparse
-import glob
 import subprocess as sp
-import multiprocessing as mp
 from pathlib import Path
 
-@TaskGenerator
+
 def coordreader(s, delim=','):
     try:
         x, y, z = map(float, s.split(delim))
@@ -17,42 +13,29 @@ def coordreader(s, delim=','):
        raise argparse.ArgumentTypeError("Center-coordinates and dimensions must be specified as 'x,y,z'")
 
 
-@TaskGenerator
-def dock_vina(box_center, box_size, exhaustiveness, receptor, ligand, ligand_name, output):
-    receptor_name = Path(receptor).stem
-    v = Vina(sf_name='vina',cpu=exhaustiveness)
-    v.set_receptor(receptor)
-    v.set_ligand_from_file(ligand)
-    v.compute_vina_maps(center=box_center, box_size=box_size)  # Don't forget to set this
-    v.dock(exhaustiveness=exhaustiveness)
-    fname = '{}/{}-{}-{:.2f}_{:.2f}_{:.2f}_{}_{}_{}_vina.pdbqt'.format(
-        output,
-        receptor_name,
-        ligand_name,
-        box_center[0],
-        box_center[1],
-        box_center[2],
-        box_size[0],
-        box_size[1],
-        box_size[2]
-    )
-    v.write_poses(fname, n_poses=1,overwrite=True)
+def intrange(s, delim=','):
+    try:
+        start_ind, end_ind = map(int, s.split(delim))
+        return start_ind, end_ind
+    except:
+        raise argparse.ArgumentTypeError('Index ranges must be specified as "start_ind,end_ind"')
+
 
 @TaskGenerator
-def dock_smina(box_center, box_size, exhaustiveness, receptor, ligand, ligand_name, output):
-    receptor_name = Path(receptor).stem
-    fname = '{}/{}-{}-{:.2f}_{:.2f}_{:.2f}_{}_{}_{}_smina.pdbqt'.format(
-        output,
-        receptor_name,
-        ligand_name,
-        box_center[0],
-        box_center[1],
-        box_center[2],
-        box_size[0],
-        box_size[1],
-        box_size[2]
-    )
-    sp.run(['smina', '--receptor', receptor, '--ligand', ligand, \
+def dock_vina(box_center, box_size, exhaustiveness, receptor_path, ligand_path, output_path):
+    v = Vina(sf_name='vina', cpu=exhaustiveness)
+    v.set_receptor(str(receptor_path))
+    v.set_ligand_from_file(str(ligand_path))
+    v.compute_vina_maps(center=box_center, box_size=box_size)
+    v.dock(exhaustiveness=exhaustiveness)
+    v.write_poses(str(output_path), n_poses=1, overwrite=True)
+    return True
+
+
+@TaskGenerator
+def dock_smina(box_center, box_size, exhaustiveness, receptor_path, ligand_path, output_path):
+    output_fn = str(output_path)
+    return sp.run(['smina', '--receptor', receptor_path, '--ligand', ligand_path, \
         '--center_x', '%s' % box_center[0], \
         '--center_y', '%s' % box_center[1], \
         '--center_z', '%s' % box_center[2], \
@@ -62,7 +45,8 @@ def dock_smina(box_center, box_size, exhaustiveness, receptor, ligand, ligand_na
         '--exhaustiveness', '%s' % exhaustiveness, \
         '--cpu', '%s' % exhaustiveness, \
         '--num_modes','1', \
-        '--out', fname])
+        '--out', output_fn], shell=True, check=True)
+
 
 docking_methods = {
     'vina': dock_vina,
@@ -71,7 +55,7 @@ docking_methods = {
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('protein_dir',
+parser.add_argument('receptor_dir',
                     help='Path to protein directory')
 parser.add_argument('out_dir',
                     help='Path to the output. By convention, the name of the docking run including info like box size '
@@ -82,15 +66,21 @@ parser.add_argument('box_size', type=coordreader,
                     help='Comma delimited string listing lx,ly,lz as the lengths of the x, y and z box-sides.')
 parser.add_argument('ligand_list', nargs="+",
                     help='Path(s) to ligand pdbqts. Alternatively a txt file with a ligand path on each line.')
-parser.add_argument('-r', '--replicas', type=int, default=1,
-                    help='Number of replica docking runs to perform. Default: 1')
+parser.add_argument('-r', '--replicas', type=intrange, default=None,
+                    help='Number of replica docking runs to perform.')
 parser.add_argument('-e', '--exhaustiveness', type=int, default=32,
-                    help='AutoDock-Vina exhaustiveness parameter. Threads used proportional to this value. Default: 32')
+                    help='AutoDock-Vina exhaustiveness parameter. Threads used proportional to this value.')
 parser.add_argument('--protein-prefix', type=str, default='frame00',
-                    help='String to prefix output pdbqts with. Default: frame00')
+                    help='String to prefix output pdbqts with.')
 parser.add_argument('-d','--docking_algorithm', default='vina',
                     choices=docking_methods.keys(),
-                    help='Pick which docking algorithm to use. Default: vina')
+                    help='Pick which docking algorithm to use.')
+parser.add_argument('-s', '--symlink-receptors', action=argparse.BooleanOptionalAction,
+                    help='Create relative symlinks for receptor PDBs into each docked ligand dir.')
+parser.add_argument('-t', '--top-dir', type=Path, default=Path.cwd(),
+                    help='Set a top directory for relative symlinks.')
+parser.add_argument('--dry-run', action=argparse.BooleanOptionalAction,
+                    help="If thrown, don't actually run docking; just create directories and (optionally) symlinks.")
 
 
 args = parser.parse_args()
@@ -102,31 +92,40 @@ if len(args.ligand_list) == 1:
         ligand_paths = args.ligand_list
 else:
     ligand_paths = args.ligand_list
-path_prot = args.protein_dir
-path_output = args.output
-n_procs = mp.cpu_count() #TODO Need to check whether this takes cpu count of the node, or the number of cpus requested
-pool = mp.Pool(processes=int(n_procs/args.exhaustiveness)) #if the above function gets the # of CPUs requested, this should work
+path_receptor = Path(args.receptor_dir)
 
-try:
-    os.makedirs('%s' % path_output)
-except FileExistsError:
-    pass
+# Figure out whether we need to index them by replica count.
+if args.replicas:
+    start_ind, end_ind = args.replicas
+    output_paths = [Path(args.out_dir + '-{}'.format(r)) for r in range(start_ind, end_ind)]
+else:
+    output_paths = [Path(args.out_dir)]
 
-for ligand in ligand_paths:
-    ligand_name = Path(ligand).stem
-    for replica in range(args.replicas):
-        try:
-            os.makedirs('%s/%s/replica%s' % (path_output,ligand_name,replica))
-        except FileExistsError:
-            pass
-        frames = sorted(glob.glob('%s/*pdbqt' % path_prot))
-        ligand_files = [i for i in [ligand] for l in range(len(frames))]
-        ligand_names = [i for i in [ligand_name] for l in range(len(frames))]
-        output_path = '%s/%s/replica%s' % (path_output,ligand_name,replica)
-        output_paths = [i for i in [output_path] for l in range(len(frames))]
-        loaded_dock = partial(docking_methods[args.docking_algorithm], args.box_center, args.box_size, args.exhaustiveness)
-        arguments = zip(frames, ligand_files, ligand_names, output_paths)
-        list(pool.starmap(loaded_dock, arguments))
+# Make output dirs.
+for p in output_paths:
+    p.mkdir(exist_ok=True, parents=True)
 
-pool.close()
+# uses recursive glob. Must be sorted to get same order across runs.
+frame_paths = [sorted(path_receptor.rglob('*.pdbqt'))]
+
+
+for run_path in output_paths:
+    for ligand in ligand_paths:
+        lig_path = Path(ligand)
+        ligand_name = lig_path.stem
+        lig_output_path = run_path / ligand_name
+        for frame_path in frame_paths:
+            docked_lig_path = lig_output_path.joinpath(*frame_path.parts[1:])
+            docked_dir_path = docked_lig_path.parent
+            if not docked_dir_path.is_dir():
+                docked_dir_path.mkdir(exist_ok=True, parents=True)
+            if not args.dry_run:
+                Task(docking_methods[args.docking_algorithm](
+                    args.box_center,
+                    args.box_size,
+                    args.exhaustiveness,
+                    frame_path,
+                    lig_path,
+                    docked_lig_path
+                ))
 
