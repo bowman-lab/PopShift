1162f785b5e032644133ac0f2aaa0d2f18ad1c30
louissmith <louis.smith88@gmail.com>
2022-04-25 16:32:43 -0500

    finished touching up extract scores for now.

--- extract-scores.py
+++ extract-scores.py
@@ -41,7 +41,6 @@ for dock_run in args.docking_runs:
         state_paths = sorted(sorted(sample_path for sample_path in state_path.iterdir())
                              for state_path in ligand_path.iterdir())
         r = ra.RaggedArray(pool.map(em, state_paths))
-        print(r._data.dtype)
         ra.save(str(out_path/ligand_path.stem)+'.h5', r)
 
 pool.close()
