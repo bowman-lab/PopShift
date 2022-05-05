3e3c1859718fb403d8f53efa0f487e6aca581f23
louissmith <louis.smith88@gmail.com>
2022-05-04 22:44:59 -0500

    Got correct two-layer structure out of sorting.

--- extract-scores.py
+++ extract-scores.py
@@ -4,7 +4,7 @@ import multiprocessing as mp
 import argparse
 import re
 from pathlib import Path
-
+import pickle
 
 def extract_score_from_vina_pdbqt(pdbqts: list, search_re=re.compile(r'^REMARK VINA RESULT')):
     outlist = []
@@ -25,22 +25,29 @@ extract_methods = {
 parser = argparse.ArgumentParser()
 parser.add_argument('docking_runs', nargs='+',
                     help='Path(s) to docked ligand directories, containing PDBQTs.')
-parser.add_argument('--nprocs','-n', type=int, default=1,
+parser.add_argument('--nprocs', '-n', type=int, default=1,
                     help='number of processors to use for multiprocessing module.')
 parser.add_argument('--result-type', '-t', choices=extract_methods.keys(), default='vina',
                     help='Which type of output file you are trying to extract a score from.')
 
 args = parser.parse_args()
 pool = mp.Pool(args.nprocs)
+number_pattern = re.compile('\d+')
 em = extract_methods[args.result_type]
 for dock_run in args.docking_runs:
     dock_path = Path(dock_run)
-    out_path = dock_path.parent/'extracted_scores'/dock_path.name
+    out_path = dock_path.parent / 'extracted_scores' / dock_path.name
     out_path.mkdir(exist_ok=True, parents=True)
     for ligand_path in dock_path.iterdir():
-        state_paths = sorted(sorted(sample_path for sample_path in state_path.iterdir())
-                             for state_path in ligand_path.iterdir())
-        r = ra.RaggedArray(pool.map(em, state_paths))
-        ra.save(str(out_path/ligand_path.stem)+'.h5', r)
+        state_paths = sorted((state_path for state_path in ligand_path.iterdir()),
+                             key=lambda x: int(x.stem))
+        result_paths = [sorted((sample_path for sample_path in state_path.rglob('*.pdbqt')),
+                               key=lambda x: tuple(map(int, number_pattern.findall(str(x))[-2:])))
+                        for state_path in state_paths]
+        r = ra.RaggedArray(pool.map(em, result_paths))
+        outpre = out_path/ligand_path.stem
+        ra.save(str(outpre.with_suffix('.h5')), r)
+        with outpre.with_suffix('.pickle').open('wb') as f:
+            pickle.dump(result_paths, f)
 
 pool.close()
