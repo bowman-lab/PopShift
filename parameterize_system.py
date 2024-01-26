8563b9a91e232bad2b162b9d448beb3064b17e48
lgsmith <louis.smith88@gmail.com>
2024-01-26 13:08:06 -0500

    Changed to reading intact ligand + gb params from offxml on disk.

--- parameterize_system.py
+++ parameterize_system.py
@@ -7,7 +7,7 @@ from openmm import XmlSerializer, CustomExternalForce
 from openmm import unit as u
 from pathlib import Path
 from math import sqrt
-
+from io import StringIO
 
 def off_serialize(outdir: Path, name, openff_obj):
     json_str = openff_obj.to_json()
@@ -23,40 +23,36 @@ def omm_serialize(outdir: Path, name, omm_obj):
     outp.write_text(xml_str)
     return outp
 
-
-# excerpted from 
-# https://github.com/jeff231li/HG-GBSA-Optimization/blob/main/paper_simulations/01-optimization/result/optimize/openff-2.0.0-GBSA_OBC2-tagged.offxml
-gb_snippet_setiadi = \
-'''<SMIRNOFF version="0.3" aromaticity_model="OEAroModel_MDL">
+gb_setiadi_fstream = StringIO(
+    """<SMIRNOFF version="0.3" aromaticity_model="OEAroModel_MDL">
     <Author>The Open Force Field Initiative AND J. Chodera, (MSKCC); J. Wagner (Open Force Field)</Author>
     <Date>2021-08-16 AND 2019-08-08</Date>
     <GBSA version="0.3" gb_model="OBC2" solvent_dielectric="78.5" solute_dielectric="1" sa_model="ACE" surface_area_penalty="5.4 * angstrom**-2 * mole**-1 * calorie" solvent_radius="1.4 * angstrom">
         <Atom smirks="[*:1]" radius="0.15 * nanometer" scale="0.8"/>
-        <Atom smirks="[#1:1]" radius="7.068228303295e-02 * nanometer" scale="0.85" parameterize="radius"/>
-        <Atom smirks="[#1:1]~[#7]" radius="1.218256113149e-01 * nanometer" scale="0.85" parameterize="radius"/>
-        <Atom smirks="[#6:1]" radius="1.662784203155e-01 * nanometer" scale="0.72" parameterize="radius"/>
-        <Atom smirks="[#7:1]" radius="5.327800106182e-02 * nanometer" scale="0.79" parameterize="radius"/>
-        <Atom smirks="[#8:1]" radius="1.632214397109e-01 * nanometer" scale="0.85" parameterize="radius"/>
+        <Atom smirks="[#1:1]" radius="7.068228303295e-02 * nanometer" scale="0.85"/>
+        <Atom smirks="[#1:1]~[#7]" radius="1.218256113149e-01 * nanometer" scale="0.85"/>
+        <Atom smirks="[#6:1]" radius="1.662784203155e-01 * nanometer" scale="0.72"/>
+        <Atom smirks="[#7:1]" radius="5.327800106182e-02 * nanometer" scale="0.79"/>
+        <Atom smirks="[#8:1]" radius="1.632214397109e-01 * nanometer" scale="0.85"/>
         <Atom smirks="[#9:1]" radius="0.15 * nanometer" scale="0.88"/>
         <Atom smirks="[#14:1]" radius="0.21 * nanometer" scale="0.8"/>
         <Atom smirks="[#15:1]" radius="0.185 * nanometer" scale="0.86"/>
         <Atom smirks="[#16:1]" radius="0.18 * nanometer" scale="0.96"/>
         <Atom smirks="[#17:1]" radius="0.17 * nanometer" scale="0.8"/>
     </GBSA>
-</SMIRNOFF>'''
-
+</SMIRNOFF>"""
+)
 
 p = ap.ArgumentParser(formatter_class=ap.ArgumentDefaultsHelpFormatter)
 p.add_argument('receptor_pdb', type=Path, 
                help='Receptor PDB file to parameterize.')
 p.add_argument('ligand_smiles', type=str,
                help='Smiles string of ligand to parameterize.')
+p.add_argument('ligand_ff', type=str,
+               help='Name of force field to use as an argument to SMIRNOFFTemplateGenerator. '
+               'Should include GB parameters for ligand.')
 p.add_argument('out_dir', type=Path,
                help='Name of directory to write parameterized jsons and systems to.')
-p.add_argument('--ligand-ff', type=str, default='openff_unconstrained-2.1.0.offxml',
-               help='Name of force field to use as an argument to SMIRNOFFTemplateGenerator.')
-p.add_argument('--ligand-implicit', type=str, default=gb_snippet_setiadi,
-               help='GB parameters to use for atoms parameterized by openff.')
 p.add_argument('--receptor-ff', type=str, default='amber/protein.ff14SB.xml',
                help='Name of force field xml to use as argument to openmm.ForceField.')
 p.add_argument('--implicit', type=str, default='implicit/obc2.xml', 
@@ -91,11 +87,10 @@ off_serialize(args.out_dir, 'receptor-top', receptor)
 off_serialize(args.out_dir, 'ligand-top', lig_top)
 off_serialize(args.out_dir, 'complex-top', rl_complex)
 
-ligand_off = off_ForceField(args.ligand_ff, args.ligand_implicit)
 # Create the SMIRNOFF template generator with the default installed force field
-smirnoff = SMIRNOFFTemplateGenerator(molecules=ligand, forcefield=ligand_off)
+smirnoff = SMIRNOFFTemplateGenerator(molecules=ligand, forcefield=args.ligand_ff)
 # Create an OpenMM ForceField object with AMBER ff14SB and TIP3P with compatible ions
-forcefield = ForceField(args.receptor_ff, args.implicit)
+forcefield = ForceField(args.receptor_ff)
 # Register the SMIRNOFF template generator
 forcefield.registerTemplateGenerator(smirnoff.generator)
 
